01d6c695d8bd2c91114ca06a79d72bf294fe012d
bidah <bartoindahouse@gmail.com>
2016-02-05 10:13:03 -0300

    done

--- js/comparison.js
+++ js/comparison.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function testFalseLooseEqualityComparison(){
+  return 1 == 0;
+}
+
+function testTrueLooseNonEqualityComparison(){
+  return 1 != 0;
+}
+
+function testTrueLooseEqualityComparison(){
+  return 1 == "1";
+}
+
+function testFalseStrictEqualityComparison(){
+  var one = 1;
+  var two = '1';
+  
+  return one === two;
+}
+
+function testTrueStrictEqualityComparison() {
+  return 1 === parseInt('1');
+}
+
+
